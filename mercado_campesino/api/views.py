df6634ea6d10dfca590288716b8b2ebcb75e5084
jjgomezr1 <jjgomezr1@eafit.edu.co>
2025-11-04 13:03:45 -0500

    OTRA correccion

--- mercado_campesino/api/views.py
+++ mercado_campesino/api/views.py
@@ -4,7 +4,7 @@ from productos.models import Producto
 
 def productos_disponibles(request):
     """Devuelve la lista de productos con stock > 0 en formato JSON."""
-    base_url = "http://54.158.38.201/"
+    base_url = "http://54.158.38.201"
 
     productos_queryset = Producto.objects.filter(stock__gt=0).values('id', 'nombre', 'stock')
 
